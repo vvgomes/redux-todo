e0ffe88b31c13d2d5fc9d84276f9c49b428a6391
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2016-10-07 01:11:47 -0400

    Adds Redux Definer and Chai and resuffles a bunch of things

---
+++ test/action.handlers.test.js
@@ -0,0 +1,124 @@
+import { addTodo, toggleTodo } from "../lib/action.handlers";
+import { expect } from "chai";
+import { always } from "ramda";
+
+describe("actionHandlers{}", () => {
+  const state = {
+    todos: [
+      {
+        id: "8c046a2d-b057-4f97-ba50-d7cacd04637e",
+        text: "wash dishes",
+        completed: true,
+        timestamp: "2016-09-08T01:47:00.490+0000"
+      },
+      {
+        id: "b3eccb22-ea89-46d1-bf5c-25c24cf3f85e",
+        text: "walk the dog",
+        completed: false,
+        timestamp: "2016-09-08T01:48:00.490+0000"
+      }
+    ]
+  };
+
+  describe("addTodo()", () => {
+    const uuidGen = always("c5cdc877-19da-48eb-99f3-983cde01379f");
+    const clock = always("2016-09-08T01:49:00.490+0000");
+
+    it("adds a new todo to the todo list", () => {
+      const action = { type: "addTodo", text: "take trash out" };
+      const newState = addTodo(state, action, uuidGen, clock);
+
+      expect(newState).to.deep.equal({
+        todos: [
+          {
+            id: "8c046a2d-b057-4f97-ba50-d7cacd04637e",
+            text: "wash dishes",
+            completed: true,
+            timestamp: "2016-09-08T01:47:00.490+0000"
+          },
+          {
+            id: "b3eccb22-ea89-46d1-bf5c-25c24cf3f85e",
+            text: "walk the dog",
+            completed: false,
+            timestamp: "2016-09-08T01:48:00.490+0000"
+          },
+          {
+            id: "c5cdc877-19da-48eb-99f3-983cde01379f",
+            text: "take trash out",
+            completed: false,
+            timestamp: "2016-09-08T01:49:00.490+0000"
+          }
+        ]
+      });
+    });
+  });
+
+  describe("toggleTodo()", () => {
+    it("marks an uncompleted todo as completed", () => {
+      const action = { type: "toggleTodo", id: "b3eccb22-ea89-46d1-bf5c-25c24cf3f85e" };
+      const newState = toggleTodo(state, action);
+
+      expect(newState).to.deep.equal({
+        todos: [
+          {
+            id: "8c046a2d-b057-4f97-ba50-d7cacd04637e",
+            text: "wash dishes",
+            completed: true,
+            timestamp: "2016-09-08T01:47:00.490+0000"
+          },
+          {
+            id: "b3eccb22-ea89-46d1-bf5c-25c24cf3f85e",
+            text: "walk the dog",
+            completed: true,
+            timestamp: "2016-09-08T01:48:00.490+0000"
+          }
+        ]
+      });
+    });
+
+    it("marks a completed todo back to uncompleted", () => {
+      const action = { type: "toggleTodo", id: "8c046a2d-b057-4f97-ba50-d7cacd04637e" };
+      const newState = toggleTodo(state, action);
+
+      expect(newState).to.deep.equal({
+        todos: [
+          {
+            id: "8c046a2d-b057-4f97-ba50-d7cacd04637e",
+            text: "wash dishes",
+            completed: false,
+            timestamp: "2016-09-08T01:47:00.490+0000"
+          },
+          {
+            id: "b3eccb22-ea89-46d1-bf5c-25c24cf3f85e",
+            text: "walk the dog",
+            completed: false,
+            timestamp: "2016-09-08T01:48:00.490+0000"
+          }
+        ]
+      });
+    });
+
+    it("does nothing when the todo cannot be found", () => {
+      const action = { type: "toggleTodo", id: "666" };
+      const newState = toggleTodo(state, action);
+
+      expect(newState).to.deep.equal({
+        todos: [
+          {
+            id: "8c046a2d-b057-4f97-ba50-d7cacd04637e",
+            text: "wash dishes",
+            completed: true,
+            timestamp: "2016-09-08T01:47:00.490+0000"
+          },
+          {
+            id: "b3eccb22-ea89-46d1-bf5c-25c24cf3f85e",
+            text: "walk the dog",
+            completed: false,
+            timestamp: "2016-09-08T01:48:00.490+0000"
+          }
+        ]
+      });
+    });
+  });
+
+});
---
+++ test/action.validators.test.js
@@ -0,0 +1,83 @@
+import { addTodo, toggleTodo } from "../lib/action.validators";
+import { expect } from "chai";
+import Validation from "data.validation";
+
+const Success = Validation.Success;
+const Failure = Validation.Failure;
+
+describe("actionValidators{}", () => {
+  const state = {
+    todos: [
+      {
+        id: "8c046a2d-b057-4f97-ba50-d7cacd04637e",
+        text: "wash dishes",
+        completed: true,
+        timestamp: "2016-09-08T01:47:00.490+0000"
+      }
+    ]
+  };
+
+  describe("addTodo()", () => {
+    it("results in success when there are no violations", () => {
+      const action = { type: "addTodo", text: "walk the dog" };
+      const result = addTodo(state, action);
+
+      expect(result).to.deep.equal(
+        Success({
+          type: "addTodo",
+          text: "walk the dog"
+        })
+      );
+    });
+
+    it("fails when the todo does not have a text description", () => {
+      const action = { type: "addTodo" };
+      const result = addTodo(state, action);
+
+      expect(result).to.deep.equal(
+        Failure(["Todo must have a text description."])
+      );
+    });
+
+    it("fails when the todo text is empty", () => {
+      const action = { type: "addTodo", text: "" };
+      const result = addTodo(state, action);
+
+      expect(result).to.deep.equal(
+        Failure(["Todo text must not be empty."])
+      );
+    });
+
+    it("fails when the todo is duplicated", () => {
+      const action = { type: "addTodo", text: "wash dishes" };
+      const result = addTodo(state, action);
+
+      expect(result).to.deep.equal(
+        Failure(["Todo text must be unique."])
+      );
+    });
+  });
+
+  describe("toggleTodo()", () => {
+    it("results in success when there are no violations", () => {
+      const action = { type: "toggleTodo", id: "8c046a2d-b057-4f97-ba50-d7cacd04637e" };
+      const result = toggleTodo(state, action);
+
+      expect(result).to.deep.equal(
+        Success({
+          type: "toggleTodo",
+          id: "8c046a2d-b057-4f97-ba50-d7cacd04637e"
+        })
+      );
+    });
+
+    it("fails when todo is cannot be found", () => {
+      const action = { type: "toggleTodo", id: "666" };
+      const result = toggleTodo(state, action);
+
+      expect(result).to.deep.equal(
+        Failure(["Todo not found."])
+      );
+    });
+  });
+});
